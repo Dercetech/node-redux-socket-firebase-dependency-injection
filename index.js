d332a23e3b2a9b9cce4296c079109fe8843b2cfd
Jérémie Mercier (e026486) <Abadon1986>
2018-08-28 16:48:32 +0200

    first master commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// NPM dependencies
-const Promise = require('bluebird');
-
-// Main start script
-const server = require('./main');
-
-// Dependencies declarations
-const firestoreDep = require('./services/firebase').getFirestoreInstance();
-
-// Start server with dependency injection \(°Ω°)/
-Promise.all([firestoreDep]).then(([firestore]) => server({ firestore }));
+require('trapezo').resolve(module, function(main) {
+  main.start();
+});
